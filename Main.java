8aaa53f700f50d28de6da081aa0c5909ce1a2629
Taryn Cail <taryncail@outlook.com>
2024-10-31 09:23:39 -0300

    updating the main method 9:23, october 31st

--- Main.java
+++ Main.java
@@ -27,22 +27,53 @@ public class Main
             System.out.println("2- Create a  group");
             System.out.println("3- Check conflicts");
             System.out.println("4- Add course to group");
+            // Should we add another display schedule option?
             System.out.println("5- To exit :) ");
             System.out.println("What would you like to do?");
             input = myScanner.nextInt();
 
             switch (input) {
                 case 1:
-                    // call create group method
+                    System.out.println("What is the courseID?");
+                    String courseID = myScanner.next();
+                    System.out.println("Is this a Lecture, Tutorial or Lab?");
+                    String extraText = myScanner.next();
+                    System.out.print("What is the time slot?");
+                    String timeSlot = myScanner.next();
+                    // CALL PARSE? store things separately in the course class?
+                    System.out.println("Is there a prof? Yes/No");
+                    if (myScanner.next().equals("Yes"))
+                    {
+                        String prof = myScanner.next();
+                        Course newCourse = new Course(courseID, extraText, timeSlot, prof);
+                    }
+                    else
+                    {
+                        Course newCourse = new Course(courseID, extraText, timeSlot);
+                    }
                     break;
                 case 2:
-                    // call create group method
+                    System.out.println("What is the group name?");
+                    String groupName = myScanner.next();
+                    Group newGroup = new Group(groupName);
                     break;
                 case 3:
-                    // call check for conflicts
+                    System.out.println("What is the name of the group?");
+                    String checkConflictGroup = myScanner.next();
+                    System.out.println("What is the courseID of the first class?");
+                    String firstClass = myScanner.next();
+                    System.out.println("What is the courseID of the second class?");
+                    String secondClass = myScanner.next();
+                    // Not sure if this is the right way to call it \/
+                    checkConflictGroup.checkConflict(firstClass, secondClass);
                     break;
                 case 4:
-                    // call add course to group
+                    System.out.println("What is the name of the group?");
+                    String thisGroup = myScanner.next();
+                    System.out.println("What is the courseID of the class?");
+                    String className = myScanner.next();
+                    // Should work? \/
+                    thisGroup.add(className);
                     break;
                 case 5:
                     done = true;
